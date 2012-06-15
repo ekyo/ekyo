8923c8f6bec3182f002d1bbcc052db266541d324
Simon Kerouack <ekyo777@gmail.com>
2012-06-15 15:35:31 -0300

    missing ;

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -71,7 +71,7 @@ apt-get install -y \
 cd ~
 apt-get install -y zsh
 [ ! -d .oh-my-zsh ] && git clone https://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh
-if [ -e .zshrc ] then
+if [ -e .zshrc ]; then
     cp .zshrc .zshrc.back
     rm .zshrc
 fi
