3ab769e96401fa42d3f16b4c96b6acfd2831353d
Simon <ekyo777@gmail.com>
2012-06-29 17:15:40 -0400

    Added prerequisites for virtualBox additions and ubuntu-tweak. The script can ONLY be run as root

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 Configure your ubuntu-server 12.04 LTS x64
 ------------
 1. Download the install/configuration script `wget https://raw.github.com/ekyo/ekyo/master/install-ubuntu.sh`
-2. Run the script`sh install-ubuntu.sh`
-  It will ask your root password and then will try configuring git for you, it will ask you
+2. Run the script`sudo sh install-ubuntu.sh`
+  It will try configuring git for you, it will ask you
     - your name, enter it like this: `"FirstName LastName"`
     - your email: `"youremail@anydomain.com"`
 3. Restart the system now `sudo shutdown -r now`
 
 After restart, log in using your account and install remaining drivers (or VirtualBox additions if your ubuntu is a guest)
+
+To activate auto-completion and a lot of other nice things, you'll need to do
+the following in emacs: `M-x byte-compile-world`
+
+
+M-x means pressing Alt and x on your keyboard in most cases.
+
--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -2,31 +2,32 @@ cd ~
 # Must be run as admin
 
 # Configure Git
-sudo apt-get -y install git
+apt-get -y install git
 read -p "Name for git?" gituser
 read -p "Email for git?" email
-sudo git config --global user.name $gituser
-sudo git config --global user.email $email
-sudo git config --global credential.helper cache
-sudo git config --global credential.helper 'cache --timeout=3600'
+git config --global user.name $gituser
+git config --global user.email $email
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=3600'
 
 # Update System and Install configuration-less Programs
-sudo apt-get -y dist-upgrade
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get install -y \
-    ubuntu-desktop \
+apt-get -y dist-upgrade
+apt-get -y update
+apt-get -y upgrade
+apt-get install -y \
+    dkms \
+    ubuntu-desktop ubuntu-tweak \
     haskell-platform haskell-platform-doc haskell-platform-prof \
     pidgin chromium-browser openssh-server
 
 # Install MongoDB
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 echo deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen \
     > /etc/apt/sources.list.d/10gen.list
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get install -y mongodb-10gen 
-sudo mongod --nojournal
+apt-get -y update
+apt-get -y upgrade
+apt-get install -y mongodb-10gen 
+mongod --nojournal
 
 # Install Redis
 #cd ~
@@ -52,13 +53,13 @@ make
     sed 's,$REDIS_LOG_FILE,/var/log/redis_6379.log,g' \
     sed 's,$REDIS_DATA_DIR,/var/redis/6379,g' \
     > /etc/redis/6379.conf
-sudo update-rc.d redis_6379 defaults
+update-rc.d redis_6379 defaults
 cd ..
 
 # Install DMD
 echo "deb http://d-apt.googlecode.com/files /" >> /etc/apt/sources.list
-sudo apt-get update && apt-get -y --allow-unauthenticated install d-apt-keyring && apt-get update
-sudo apt-get install -y \
+apt-get update && apt-get -y --allow-unauthenticated install d-apt-keyring && apt-get update
+apt-get install -y \
     dmd \
     libgtkd-dev libgtkd-doc \
     libplot2kill-dev libplot2kill-doc \
@@ -74,7 +75,7 @@ sudo apt-get install -y \
 
 # Install Zsh with Oh My Zsh
 #cd ~
-sudo apt-get install -y zsh
+apt-get install -y zsh
 [ ! -d .oh-my-zsh ] && git clone https://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh
 if [ -e .zshrc ]; then
     cp .zshrc .zshrc.back
@@ -86,7 +87,7 @@ chsh -s /usr/bin/zsh
 
 # Install emacs
 #cd ~
-sudo apt-get install -y emacs
+apt-get install -y emacs
 [ ! -d .dude-dot-files ] && \
     git clone --recursive git://github.com/ddude/dude-dot-files.git .dude-dot-files
 [ ! -f .emacs ] && \
@@ -99,11 +100,11 @@ sudo apt-get install -y emacs
     ln -s .dude-dot-files/.bash_profile .bash_profile
 
 # Install nodejs
-sudo apt-get install -y python-software-properties
-sudo apt-add-repository -y ppa:chris-lea/node.js
-sudo apt-get update
-sudo apt-get install -y nodejs npm
-sudo apt-get install -y nodejs-dev
+apt-get install -y python-software-properties
+apt-add-repository -y ppa:chris-lea/node.js
+apt-get update
+apt-get install -y nodejs npm
+apt-get install -y nodejs-dev
 
 # Install coffee-script
-sudo npm install -g coffee-script
+npm install -g coffee-script
