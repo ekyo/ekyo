7dc11ac7f9ca03a366607609107b16210659f415
Simon <ekyo777@gmail.com>
2012-08-10 13:12:22 -0400

    added installation script for ruhoh

---
+++ shell-scripts/setup-ruhoh.sh
@@ -0,0 +1,10 @@
+sudo apt-get install -y \
+    libyaml-dev \
+    libxml2-dev \
+    libxslt1-dev \
+    ruby1.9.3 \
+    rubygems
+
+sudo gem install ruhoh --version '1.0.0.alpha'
+
+sudo gem install rack
