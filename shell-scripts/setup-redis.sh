6a76b163668c2073fcf8af15103afc9c2ec28749
Simon Kerouack <ekyo777@gmail.com>
2012-12-10 14:00:47 -0500

    updated redis script

--- shell-scripts/setup-redis.sh
+++ shell-scripts/setup-redis.sh
@@ -1,8 +1,8 @@
 # Install Redis
 cd ~
-wget http://redis.googlecode.com/files/redis-2.6.6.tar.gz
-tar xzf redis-2.6.6.tar.gz
-cd redis-2.6.6
+wget http://redis.googlecode.com/files/redis-2.6.7.tar.gz
+tar xzf redis-2.6.7.tar.gz
+cd redis-2.6.7
 make
 sudo make install
 [ ! -f "/usr/local/bin/redis-server" ] && sudo cp redis-server /usr/local/bin/
