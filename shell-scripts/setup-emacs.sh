092fdd2917d1c958a63c3010ca26d3ea908e7a5d
Simon Kerouack <ekyo777@gmail.com>
2013-02-27 17:10:03 -0500

    get emacs from an existing repo instead of building it from src

--- shell-scripts/setup-emacs.sh
+++ shell-scripts/setup-emacs.sh
@@ -4,16 +4,11 @@ sudo apt-get install -y \
     libxpm-dev libjpeg-dev libgif-dev libtiff4-dev libgtk2.0-dev \
     libncurses-dev libncurses5-dev \
     aspell ack wmctrl fonts-inconsolata
-wget http://ftp.gnu.org/pub/gnu/emacs/emacs-24.1.tar.gz
-tar -xzf emacs-24.1.tar.gz
-cd emacs-24.1
-./configure
-make
-sudo make install
-gsettings set com.canonical.Unity.Launcher favorites "$(gsettings get com.canonical.Unity.Launcher favorites | sed "s/, *'emacs' *//g" | sed "s/'emacs' *, *//g" | sed -e "s/]$/, 'emacs']/")"
-cd ..
-rm -r emacs-24.1
-rm emacs-24.1.tar.gz
+
+sudo add-apt-repository ppa:cassou/emacs
+sudo apt-get update
+sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
+
 # Prelude Extension
 curl -L https://github.com/bbatsov/prelude/raw/master/utils/installer.sh | sh
 
