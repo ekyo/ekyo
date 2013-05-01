633cf283267b2f8269cccfa844f7aec7ac9ba34c
Simon Kerouack <ekyo777@gmail.com>
2013-05-01 10:18:14 -0400

    dependencies for howdoi

--- shell-scripts/setup-general.sh
+++ shell-scripts/setup-general.sh
@@ -3,6 +3,7 @@ sudo apt-get update
 sudo apt-get upgrade
 
 sudo apt-get install -y \
+    python-pip python-dev build-essential \
     htop cloc \
     dkms screen \
     haskell-platform haskell-platform-doc haskell-platform-prof hlint \
@@ -10,4 +11,6 @@ sudo apt-get install -y \
     git meld ack-grep \
     pidgin pidgin-skype chromium-browser openssh-server
 
+sudo pip install --upgrade pip
+sudo pip install --upgrade virtualenv
 sudo pip install howdoi
