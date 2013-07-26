78914570077f06fe99184faa0bf95f28267dbff9
Simon <ekyo777@gmail.com>
2013-07-26 11:28:59 -0400

    use patience algorithm for diff by default

--- shell-scripts/setup-git.sh
+++ shell-scripts/setup-git.sh
@@ -4,3 +4,4 @@ read -p "Name for git? " gituser
 read -p "Email for git? " email
 git config --global user.name $gituser
 git config --global user.email $email
+git config --global diff.algorithm patience
