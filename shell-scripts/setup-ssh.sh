e8a73df631f1f9a20a481dbac4f521a13160397f
Simon <ekyo777@gmail.com>
2013-07-26 11:23:57 -0400

    added mosh to ssh setup

--- shell-scripts/setup-ssh.sh
+++ shell-scripts/setup-ssh.sh
@@ -1,7 +1,14 @@
 cd ~
-mkdir .ssh
 sudo apt-get install pssh
 
+# install mosh
+sudo apt-get install -y python-software-properties
+sudo add-apt-repository ppa:keithw/mosh
+sudo apt-get update
+sudo apt-get install -y mosh
+
+# setup ssh keys
+mkdir .ssh
 cd .ssh
 touch authorized_keys2
 chmod 600 authorized_keys2
